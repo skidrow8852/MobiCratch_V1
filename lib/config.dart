edac61699f69260afd7723274cbdc1d771973d41
Youssef Rais <94503407+skidrow8852@users.noreply.github.com>
2024-12-15 00:09:26 +0100

    Update config.dart

--- lib/config.dart
+++ lib/config.dart
@@ -1,7 +1,7 @@
 const String ipfsKey =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweEY4RDllNDY5M2FiOTc5QmEyMzFDNkExRDA0NWVlZjE4QjFBODhlRmYiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2NDI1Mzk5MzI0MzksIm5hbWUiOiJmaWxlcyJ9.RuyB5HD5jn_xCl6pSJdNWJHUu_la07x0-SqWD_kfACE";
-const String walletConnectId =
-    'e9f3e234a4b76111891214acde3f9f35'; // 15faabd2ceb2a486be25c61d1b5a587a   /// e9f3e234a4b76111891214acde3f9f35
+    "";
+const String walletConnectId ="";
+    
 const String tokenContractAddress =
     "0x8E6F876e4d04FE5516FF3215F509E82eB3083738";
 const String contractAbiPath = "assets/images/contracts/abi.json";
